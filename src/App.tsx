0dc6649bdef098e0e1ee632661f13bef20b79ba0
John Edmondson <edmondsonj@gmail.com>
2023-03-06 11:54:59 -0800

    convert project to TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// Dependencies
+import React from "react";
+
+// Styles
+import "./App.css";
+
+// Components
+
+const App = () => {
+  return (
+    <div className="App">
+      <p>Portfolio Site Coming Soon</p>
+    </div>
+  );
+};
+export default App;
