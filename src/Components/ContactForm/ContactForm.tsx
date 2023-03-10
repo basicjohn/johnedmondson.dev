a719ae9d3afc2d50492ff4ef6c8755fa23afa001
John Edmondson <edmondsonj@gmail.com>
2023-03-10 12:09:45 -0800

    add basic contact form

--- src/Components/ContactForm/ContactForm.tsx
+++ src/Components/ContactForm/ContactForm.tsx
@@ -6,6 +6,62 @@ import styles from "./ComingSoon.module.scss";
 
 const rootClass = "contact-form";
 
-const ContactForm = () => <div className={styles[rootClass]}></div>;
+const ContactForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      inquiryType: "",
+      message: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  const inquiryTypes = ["General Inquiry", "Job Inquiry", "Other"];
+
+  return (
+    <div className={styles[rootClass]}>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <label htmlFor="inquiryType">Inquiry Type</label>
+        <select
+          id="inquiryType"
+          name="inquiryType"
+          onChange={formik.handleChange}
+          value={formik.values.inquiryType}
+        >
+          <option value="" label="Select an inquiry type" />
+          {inquiryTypes.map((inquiryType) => (
+            <option value={inquiryType} label={inquiryType} />
+          ))}
+        </select>
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          name="message"
+          onChange={formik.handleChange}
+          value={formik.values.message}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default ContactForm;
