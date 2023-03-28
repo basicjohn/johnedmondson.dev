dc685b697ab508c08d807013af4c6f5946b36d37
John Edmondson <edmondsonj@gmail.com>
2023-03-28 15:24:15 -0700

    fix capitalization

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
-import Footer from "../../components/Footer/Footer";
-import Header from "../../components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+import Header from "../../Components/Header/Header";
 
 interface HomeProps {}
 
