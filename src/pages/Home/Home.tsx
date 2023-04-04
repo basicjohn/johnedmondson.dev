f1348c6b9c2d3ccd949b89755fa8b3cc78d9494e
John Edmondson <edmondsonj@gmail.com>
2023-04-03 22:30:31 -0700

    add placeholder content

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -17,6 +17,22 @@ const Home: React.FC<HomeProps> = () => {
           moonlighting to build apps (3) to solve some of the challenges I've
           faced in my personal & professional life.
         </p>
+        <h1>Placeholder Title</h1>
+        <button>Portfolio Item 1</button>
+        <p>
+          Content for Portfolio Item 1. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nunc nisl
+        </p>
+        <button>Portfolio Item 1</button>
+        <p>
+          Content for Portfolio Item 1. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nunc nisl
+        </p>
+        <button>Portfolio Item 1</button>
+        <p>
+          Content for Portfolio Item 1. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Sed euismod, nisl vel tincidunt lacinia, nunc nisl
+        </p>
         {/* Your page content goes here */}
       </Container>
       <Footer />
