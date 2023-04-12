d5ae3166868290bd83b340bd4d40c7c759de754c
John Edmondson <edmondsonj@gmail.com>
2023-04-11 17:11:30 -0700

    fix test

--- src/Components/Header/__tests__/Header.test.tsx
+++ src/Components/Header/__tests__/Header.test.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Header from "../Header";
+import "@testing-library/jest-dom/extend-expect";
+import Header from "./../Header";
 
-describe("Header component", () => {
-  test("renders portfolio title", () => {
-    render(<Header currentTitle="Portfolio" />);
-    const titleElement = screen.getByText(/Portfolio/i);
-    expect(titleElement).toBeInTheDocument();
+describe("Header Component", () => {
+  it("renders without crashing", () => {
+    const currentTitle = "John's Portfolio";
+    render(<Header currentTitle={currentTitle} />);
+    expect(
+      screen.getByAltText("John Edmondson Ilustrated Icon")
+    ).toBeInTheDocument();
+  });
+
+  it("displays social media icons", () => {
+    const currentTitle = "John's Portfolio";
+    render(<Header currentTitle={currentTitle} />);
+    expect(screen.getByAltText("LinkedIn")).toBeInTheDocument();
+    expect(screen.getByAltText("GitHub")).toBeInTheDocument();
+    expect(screen.getByAltText("Twitter")).toBeInTheDocument();
+    expect(screen.getByAltText("Pinterest")).toBeInTheDocument();
+  });
+
+  it("contains a 'Let's Chat!' button", () => {
+    const currentTitle = "John's Portfolio";
+    render(<Header currentTitle={currentTitle} />);
+    expect(screen.getByText("Let's Chat!")).toBeInTheDocument();
   });
 });
