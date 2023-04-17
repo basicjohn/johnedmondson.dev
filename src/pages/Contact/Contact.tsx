691e81f92cc2cd3d3d24bdb3fed8a16656c73bf6
John Edmondson <edmondsonj@gmail.com>
2023-04-17 16:50:17 -0700

    add schedule meeting to contact form

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,7 +1,9 @@
-import styles from "./Contact.module.scss";
+import { Button } from "@mui/material";
 
 import ContactForm from "Components/ContactForm/ContactForm";
 import Greeting from "Components/Greeting/Greeting";
+
+import styles from "./Contact.module.scss";
 
 const rootClass = "contact-page";
 
@@ -10,6 +12,16 @@ const Contact = () => {
     <div className={styles[rootClass]}>
       <div className={styles[`${rootClass}__left-column`]}>
         <Greeting />
+        <Button
+          variant="contained"
+          component="a"
+          target="_blank"
+          href="https://calendly.com/johnedmondsondev/lets-chat"
+          rel="noreferrer"
+          className={styles[`${rootClass}__button`]}
+        >
+          Schedule a Meeting
+        </Button>
       </div>
       <div className={styles[`${rootClass}__right-column`]}>
         <ContactForm />
