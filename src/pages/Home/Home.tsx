e46a7b8869a3a933cf97a472645a02b1c13e5d1b
John Edmondson <edmondsonj@gmail.com>
2023-05-03 13:41:02 -0700

    WIP: styling & copy

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -13,53 +13,55 @@ const Home = () => {
           <section id="about" className={styles[`${rootClass}__about`]}>
             <h1>
               Hello, I'm John <br />
-              <span>and I make things</span>
             </h1>
-            <h2>Software Engineer & Maker</h2>
+            <h2>I make things</h2>
+            <h3>Software Engineer & Maker</h3>
           </section>
         </div>
+        <h2>Skills</h2>
       </Container>
+
       <SkillTicker />
       <Container>
-        <div className={styles[rootClass]}>
-          {/* <section className={styles[`${rootClass}__skills`]}></section> */}
-          <section id="portfolio" className={styles[`${rootClass}__portfolio`]}>
-            <h1>Code Samples</h1>
-            <h3>
-              Animated background in the style of 90's geometric art using
-              three.js
-            </h3>
-            <a
-              href="https://codesandbox.io/p/sandbox/nifty-black-hqtk8p?file=%2Fsrc%2FApp.tsx&selection=%5B%7B%22endColumn%22%3A11%2C%22endLineNumber%22%3A11%2C%22startColumn%22%3A11%2C%22startLineNumber%22%3A11%7D%5D"
-              target="_blank"
-              rel="noreferrer"
-            >
-              CodeSandbox
-            </a>
-            <hr className={styles[`${rootClass}__short-rule`]} />
-            <h3>Repository for johnedmondson.dev</h3>
-            <a
-              href="https://codesandbox.io/p/sandbox/nifty-black-hqtk8p?file=%2Fsrc%2FApp.tsx&selection=%5B%7B%22endColumn%22%3A11%2C%22endLineNumber%22%3A11%2C%22startColumn%22%3A11%2C%22startLineNumber%22%3A11%7D%5D"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github
-            </a>
-            <hr className={styles[`${rootClass}__short-rule`]} />
-            <h3>
-              Animated background in the style of 90's geometric art using
-              three.js
-            </h3>
-            <a
-              href="https://github.com/stars/basicjohn/lists/coding-bootcamp-solo-projects"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Github Repository List
-            </a>
-            <hr className={styles[`${rootClass}__short-rule`]} />
-          </section>
-        </div>
+        <section id="portfolio" className={styles[`${rootClass}__portfolio`]}>
+          <h2>Apps & Portfolio</h2>
+          <h3>johnedmondson.dev</h3>
+          <a
+            href="https://codesandbox.io/p/sandbox/nifty-black-hqtk8p?file=%2Fsrc%2FApp.tsx&selection=%5B%7B%22endColumn%22%3A11%2C%22endLineNumber%22%3A11%2C%22startColumn%22%3A11%2C%22startLineNumber%22%3A11%7D%5D"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Repository
+          </a>
+          <hr className={styles[`${rootClass}__short-rule`]} />
+          <h3>90's Style Animated Background</h3>
+          <a
+            href="https://codesandbox.io/p/sandbox/nifty-black-hqtk8p?file=%2Fsrc%2FApp.tsx&selection=%5B%7B%22endColumn%22%3A11%2C%22endLineNumber%22%3A11%2C%22startColumn%22%3A11%2C%22startLineNumber%22%3A11%7D%5D"
+            target="_blank"
+            rel="noreferrer"
+          >
+            CodeSandbox
+          </a>
+          <hr className={styles[`${rootClass}__short-rule`]} />
+          <h3>Student Projects from Coding Bootcamp</h3>
+          <a
+            href="https://github.com/stars/basicjohn/lists/coding-bootcamp-solo-projects"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Full Repository List
+          </a>
+
+          <a
+            href="https://github.com/stars/basicjohn/lists/coding-bootcamp-solo-projects"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Highlighted
+          </a>
+
+          <hr className={styles[`${rootClass}__short-rule`]} />
+        </section>
         <CallToAction />
       </Container>
     </>
