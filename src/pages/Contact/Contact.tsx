97017a470133d3f035551b48cc4bbff9aac9045f
John Edmondson <edmondsonj@gmail.com>
2023-06-02 14:22:08 -0700

    SEO & Accessibility

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,3 +1,6 @@
+// dependencies
+import { Helmet } from "react-helmet";
+
 // components
 import { Button } from "@mui/material";
 import ContactForm from "Components/ContactForm/ContactForm";
@@ -13,21 +16,34 @@ const rootClass = "contact-page";
 
 const Contact = () => {
   return (
-    <div className={styles[rootClass]}>
+    <main className={styles[rootClass]}>
+      <Helmet>
+        <title>Contact John Edmondson</title>
+        <meta
+          name="description"
+          content="Get in touch with John Edmondson, a Software Engineer based out of Portland, Oregon."
+        />
+      </Helmet>
       <h2>I hear you want to track me down...</h2>
       <h3>Here's where to start</h3>
-      <h5>Find me on the socials</h5>
+      <h4>Find me on the socials</h4>
       <div className={styles[`${rootClass}__social`]}>
         {/* LinkedIn */}
         <a
           href="https://www.linkedin.com/in/johnedmondsondev/"
           target="_blank"
           rel="noreferrer"
+          aria-label="John Edmondson's LinkedIn profile"
         >
           <img src={linkedinIcon} alt="LinkedIn" width="24px" height="24px" />
         </a>
         {/* GitHub */}
-        <a href="https://github.com/basicjohn" target="_blank" rel="noreferrer">
+        <a
+          href="https://github.com/basicjohn"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="John Edmondson's GitHub profile"
+        >
           <img src={githubIcon} alt="GitHub" width="24px" height="24px" />
         </a>
         {/* Twitter */}
@@ -35,6 +51,7 @@ const Contact = () => {
           href="https://twitter.com/basicjohn"
           target="_blank"
           rel="noreferrer"
+          aria-label="John Edmondson's Twitter profile"
         >
           <img src={twitterIcon} alt="Twitter" width="24px" height="24px" />
         </a>
@@ -43,12 +60,13 @@ const Contact = () => {
           href="https://www.pinterest.com/basicjohn/"
           target="_blank"
           rel="noreferrer"
+          aria-label="John Edmondson's Pinterest profile"
         >
           <img src={pinterestIcon} alt="Pinterest" width="24px" height="24px" />
         </a>
       </div>
 
-      <h5>Schedule a meeting with me</h5>
+      <h4>Schedule a meeting with me</h4>
       <span>
         <Button
           variant="contained"
@@ -57,6 +75,7 @@ const Contact = () => {
           href="https://calendly.com/johnedmondsondev/lets-chat"
           rel="noreferrer"
           className={styles[`${rootClass}__button`]}
+          aria-label="Schedule a meeting with John Edmondson on Calendly"
         >
           View Calendar
         </Button>{" "}
@@ -65,7 +84,7 @@ const Contact = () => {
       <hr />
       <Greeting />
       <ContactForm />
-    </div>
+    </main>
   );
 };
 
