4381a9b7dd78a0d14f1e1efe30c1c8f9930ac61e
John Edmondson <edmondsonj@gmail.com>
2023-06-05 11:09:30 -0700

    update header & footer tests

--- src/Components/Footer/__tests__/Footer.test.tsx
+++ src/Components/Footer/__tests__/Footer.test.tsx
@@ -1,75 +1,119 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import Footer from "../Footer";
-
-// Mock InspirationalQuote component, as it's not being tested here
-jest.mock("Components/InspirationalQuote/InspirationalQuote", () => () => (
-  <div>InspirationalQuote</div>
-));
 
 describe("Footer", () => {
   it("renders without crashing", () => {
     render(
-      <Router>
+      <MemoryRouter>
         <Footer />
-      </Router>
+      </MemoryRouter>
     );
   });
 
-  it("displays the correct year in the copyright", () => {
+  it("has links to the correct pages", () => {
     render(
-      <Router>
+      <MemoryRouter>
         <Footer />
-      </Router>
+      </MemoryRouter>
     );
-    const date = new Date();
-    const year = date.getFullYear();
-    expect(
-      screen.getByText(`© ${year} John Edmondson. All rights reserved.`)
-    ).toBeInTheDocument();
-  });
 
-  it("contains the correct navigation links", () => {
-    render(
-      <Router>
-        <Footer />
-      </Router>
-    );
-    expect(screen.getByRole("link", { name: /about/i })).toHaveAttribute(
+    const aboutLink = screen.getByRole("link", { name: /about/i });
+    expect(aboutLink).toBeInTheDocument();
+    expect(aboutLink).toHaveAttribute("href", "/#about");
+
+    const portfolioLink = screen.getByRole("link", { name: /apps & portfolio/i });
+    expect(portfolioLink).toBeInTheDocument();
+    expect(portfolioLink).toHaveAttribute("href", "/#portfolio");
+
+    const contactLink = screen.getByRole("link", { name: /contact/i });
+    expect(contactLink).toBeInTheDocument();
+    expect(contactLink).toHaveAttribute("href", "/contact");
+
+    const scheduleLink = screen.getByRole("link", { name: /schedule appointment/i });
+    expect(scheduleLink).toBeInTheDocument();
+    expect(scheduleLink).toHaveAttribute(
       "href",
-      "/#about"
+      "https://calendly.com/johnedmondsondev/lets-chat"
     );
-    expect(
-      screen.getByRole("link", { name: /apps & portfolio/i })
-    ).toHaveAttribute("href", "/#portfolio");
-    expect(screen.getByRole("link", { name: /contact/i })).toHaveAttribute(
+
+    const repositoryLink = screen.getByRole("link", { name: /view repository/i });
+    expect(repositoryLink).toBeInTheDocument();
+    expect(repositoryLink).toHaveAttribute(
       "href",
-      "/contact"
+      "https://github.com/basicjohn/johnedmondson.dev"
     );
   });
 
-  it("contains the correct social links", () => {
+  it("has links to social media profiles", () => {
     render(
-      <Router>
+      <MemoryRouter>
         <Footer />
-      </Router>
+      </MemoryRouter>
     );
-    expect(screen.getByRole("link", { name: /linkedin/i })).toHaveAttribute(
+
+    const linkedInLink = screen.getByTitle("John's LinkedIn");
+    expect(linkedInLink).toBeInTheDocument();
+    expect(linkedInLink).toHaveAttribute(
       "href",
       "https://www.linkedin.com/in/johnedmondsondev/"
     );
-    expect(screen.getByRole("link", { name: /github/i })).toHaveAttribute(
-      "href",
-      "https://github.com/basicjohn"
+
+    const gitHubLink = screen.getByTitle("John's GitHub");
+    expect(gitHubLink).toBeInTheDocument();
+    expect(gitHubLink).toHaveAttribute("href", "https://github.com/basicjohn");
+
+    const twitterLink = screen.getByTitle("John's Twitter");
+    expect(twitterLink).toBeInTheDocument();
+    expect(twitterLink).toHaveAttribute("href", "https://twitter.com/basicjohn");
+
+    const pinterestLink = screen.getByTitle("John's Pinterest");
+    expect(pinterestLink).toBeInTheDocument();
+    expect(pinterestLink).toHaveAttribute("href", "https://www.pinterest.com/basicjohn/");
+  });
+
+  it("displays the newsletter subscription form", () => {
+    render(
+      <MemoryRouter>
+        <Footer />
+      </MemoryRouter>
     );
-    expect(screen.getByRole("link", { name: /twitter/i })).toHaveAttribute(
-      "href",
-      "https://twitter.com/basicjohn"
+
+    const newsletterIframe = screen.getByTitle("Newsletter");
+    expect(newsletterIframe).toBeInTheDocument();
+    expect(newsletterIframe).toHaveAttribute(
+      "src",
+      "https://basicjohn.substack.com/embed"
     );
-    expect(screen.getByRole("link", { name: /pinterest/i })).toHaveAttribute(
-      "href",
-      "https://www.pinterest.com/basicjohn/"
+  });
+
+  it("displays the inspirational quote", () => {
+    render(
+      <MemoryRouter>
+        <Footer />
+      </MemoryRouter>
     );
+
+    const quoteElement = screen.getByText(/"Believe you can and you're halfway there."/i);
+    expect(quoteElement).toBeInTheDocument();
+  });
+
+  it("displays the correct copyright information", () => {
+    render(
+      <MemoryRouter>
+        <Footer />
+      </MemoryRouter>
+    );
+
+    const profilePhotoElement = screen.getByAltText(
+      "Illustrated icon of John Edmondson"
+    );
+    expect(profilePhotoElement).toBeInTheDocument();
+
+    const year = new Date().getFullYear();
+    const copyrightElement = screen.getByText(
+      `© ${year} John Edmondson. All rights reserved.`
+    );
+    expect(profilePhotoElement).toBeInTheDocument();
   });
 });
