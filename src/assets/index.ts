4cf55afa7c441d02753715fdfe219ef489b8a0a0
John Edmondson <edmondsonj@gmail.com>
2023-11-27 16:53:24 -0800

    add non-contact images to the new page

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,10 +2,14 @@
 import previewImage from './preview-image.jpg';
 import profilePhoto from "./profile_photo.png";
 
+// portfolio images
+import nonContact1a from "./portfolio/non-contact/nonContactTechnologies-910a.jpg";
+import nonContact1b from "./portfolio/non-contact/nonContactTechnologies-910b.jpg";
+
 // icons
 import githubIcon from "./icons/github-48.png";
 import linkedinIcon from "./icons/linkedin-48.png";
 import pinterestIcon from "./icons/pinterest-48.png";
 import twitterIcon from "./icons/twitter-48.png";
 
-export { previewImage, profilePhoto, githubIcon, linkedinIcon, pinterestIcon, twitterIcon };
+export {  githubIcon, linkedinIcon, nonContact1a, nonContact1b, pinterestIcon, previewImage, profilePhoto, twitterIcon };
--- src/pages/portfolio/NonContactTech/NonContactTech.tsx
+++ src/pages/portfolio/NonContactTech/NonContactTech.tsx
@@ -4,6 +4,9 @@ import { Helmet } from "react-helmet";
 // components
 // import { Button } from "@mui/material";
 
+// assets
+import { nonContact1a, nonContact1b } from "assets";
+
 // styles
 import styles from "./NonContactTech.module.scss";
 
@@ -16,7 +19,9 @@ const NonContactTech = () => {
         <title>Portfolio - Non-Contact Technologies</title>
         <meta name="description" content="" />
       </Helmet>
-      <h1>Non-Contact Technologies</h1>
+      {/* <h1>Non-Contact Technologies</h1> */}
+      <img width="100%" src={nonContact1a} alt="" />
+      <img width="100%" src={nonContact1b} alt="" />
     </main>
   );
 };
